a427ba6460d0f6cfb0566448472edd34ab308dcf
den4592 <50400631+den4592@users.noreply.github.com>
2019-11-22 10:24:10 +0900

    Add files via upload

---
+++ Write.java
@@ -0,0 +1,27 @@
+package com.example.dbtest;
+
+public class Write {
+    public String texttitle;
+    public String usertext;
+    public String editdetail;
+
+    public Write(){
+
+    }
+
+    public Write(String texttitle, String usertext, String editdetail){
+        this.texttitle=texttitle;
+        this.usertext=usertext;
+        this.editdetail=editdetail;
+    }
+
+    public String getTexttitle() {return texttitle; }
+
+    public String getUsertext() {
+        return usertext;
+    }
+
+    public String getEditdetail() {
+        return editdetail;
+    }
+}
