2b6737d91b131bfac1d7c8711dfd14af7cad75ec
den4592 <50400631+den4592@users.noreply.github.com>
2019-11-22 10:53:14 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -4,45 +4,63 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txtemail,txtpassword;
-    private Button btlogout;
-    Register register=new Register();
-    private FirebaseAuth firebaseAuth;
-    @Override
 
+    private Button btnlogout;
+    private FirebaseAuth firebaseAuth;
+    private Button mypage;
+    private Button imageup;
+    private Button center;
+
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseAuth=FirebaseAuth.getInstance();
-        //if already login then do first log out then again login
-        if (firebaseAuth.getCurrentUser()==null){
-            finish();
-            startActivity(new Intent(MainActivity.this,Login.class));
-        }
+        imageup = findViewById(R.id.imageup);
+        mypage=(Button)findViewById(R.id.mypage);
+        center=(Button)findViewById(R.id.center);
 
-        FirebaseUser user =firebaseAuth.getCurrentUser();
+        center.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Center.class);
+                startActivity(intent);
+            }
+        });
+
+        mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, MyPage.class);
+                startActivity(intent);
+            }
+        });
+
+        imageup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Image.class);
+                startActivity(intent);
+            }
+        });
 
 
-        txtemail=(TextView)findViewById(R.id.email);
-
-        btlogout=(Button)findViewById(R.id.blogout);
-
-        txtemail.setText("반갑습니다!:  "+user.getEmail());
+        firebaseAuth = FirebaseAuth.getInstance();
 
 
 
-
-        btlogout.setOnClickListener(new View.OnClickListener() {
+        btnlogout = (Button) findViewById(R.id.btlogout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 firebaseAuth.signOut();
@@ -50,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Login.class));
             }
         });
+
 
     }
 }
